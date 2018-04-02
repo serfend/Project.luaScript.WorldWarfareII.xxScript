b44c06a852a156961c9755a044e9e7fe045f343a
aserfend <liuchangm7j@vip.qq.com>
2018-04-02 20:48:29 +0800

    废弃原生ui

--- xsp/src/Form.lua
+++ xsp/src/Form.lua
@@ -9,14 +9,27 @@ function Form:new (o)
 end
 
 function Form:Exit()
-	x,y=Form:findExit()
-	if x>-1 then 
-		ShowInfo.RunningInfo("退出界面")
+	return Form:DoAction(Form.findExit,"退出界面")
+end
+function Form:Submit()
+	return Form:DoAction(Form.findSubmit,"提交")
+end
+function Form:DoAction(fun,id)
+	x,y=fun()
+	if x>-1 then
+		ShowInfo.RunningInfo(id)
 		tap(x,y)
 		sleepWithCheckLoading(500)
+		return true
 	else
-	
+		return false
 	end
+end
+function Form:findSubmit()
+	return findColor({1553, 978, 1916, 1076}, 
+"0|0|0xf9b61c,1|38|0xf6ab15,240|43|0xf9b50f,238|-25|0xfec91d,140|45|0xffd201,129|-24|0xfdc81d",
+90, 0, 0, 0)
+
 end
 function Form:findExit()
 	return findColor({1764, 0, 1919, 77}, 
